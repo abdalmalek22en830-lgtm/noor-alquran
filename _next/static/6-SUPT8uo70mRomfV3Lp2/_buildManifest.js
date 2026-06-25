63219008966879430abfb35eb1139cf217da78ae
abdalmalek22en830-lgtm <abdalmalek.22en830@student.uomosul.edu.iq>
2026-06-25 13:26:20 +0300

    Updates

--- _next/static/6-SUPT8uo70mRomfV3Lp2/_buildManifest.js
+++ _next/static/6-SUPT8uo70mRomfV3Lp2/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/noor-alquran//_next/:path+",
+        "destination": "/noor-alquran/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
